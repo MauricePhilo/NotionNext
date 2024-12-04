f8822c7f3fcca081598ca66cd542b253d23ad6c6
MauricePhilo <mauricephiloso@gmail.com>
2024-12-04 19:32:39 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,11 +9,7 @@ export default function PoweredBy(props) {
     <div
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
       <span>Powered by</span>
-      <a
-        href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
-      </a>
+      D66.cn
       .
     </div>
   )
